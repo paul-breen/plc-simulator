409b81b1e2e33de52928c90df80c61a472f4f84d
Paul Breen <pbree@bas.ac.uk>
2018-07-17 12:36:38 +0100

    Ensure the ModbusModule receives the full message length
    The FieldbusMessage class now has a flexible method for robustly
    receiving complete fieldbus messages.

--- plcsimulator/FieldbusMessage.py
+++ plcsimulator/FieldbusMessage.py
@@ -5,6 +5,9 @@
 # Date:    2018-07-11
 ###############################################################################
 
+import time
+import socket
+
 class FieldbusMessage(object):
 
     DEFAULTS = {
@@ -31,4 +34,73 @@ class FieldbusMessage(object):
         word = int.from_bytes(buf_bytes, byteorder=self.DEFAULTS['byteorder'])
 
         return word
+
+    def reset_buffer(self):
+        """
+        Reset the buffer
+        """
+
+        self.buf = bytearray(self.blen)
+
+    def recv_fragment(self, conn, nbytes, timeout=None, ntries=1, pause=0,
+                      flags=0):
+        """
+        Receive a fragment of an incoming message into the buffer
+
+        Example:
+
+        Say we have a message header of 8 bytes, that contains the length of
+        a varying data payload.  The length is in byte 4.  We first read the
+        header fragment, calculate the total message size (header + data), and
+        then append the data fragment onto the buffer
+
+          msg.recv_fragment(conn, 8)
+          msg.recv_fragment(conn, 8 + msg.buf[3])
+
+        :param conn: The socket to receive the message on
+        :type conn: socket.socket
+        :param nbytes: The total required bytes of the whole message.  The
+                       number of bytes to read in this call is calculated as
+                       the difference of nbytes and the length of the buffer
+                       from any earlier calls to this function
+        :type nbytes: int
+        :param timeout: The timeout (s) to read data from the socket.  If
+                        timeout is 0, then socket is in non-blocking mode,
+                        if timeout is None, socket is in blocking mode
+        :type timeout: float or None
+        :param ntries: The maximum number of tries to read the message fragment
+        :type ntries: int
+        :param pause: The time to pause (s) between tries
+        :type pause: float
+        :param flags: Flags bitmask for the recv() call
+        :type flags: int
+        """
+
+        # Set a timeout to make calls to recv() non-blocking
+        if timeout is not None:
+            orig_timeout = conn.gettimeout()
+            conn.settimeout(timeout)
+
+        for i in range(ntries):
+            try:
+                nbytes_left = nbytes - len(self.buf)
+
+                if nbytes_left < 1:
+                    break
+
+                # Append this message fragment to the total message buffer
+                fragment = conn.recv(nbytes_left, flags)
+                self.buf += bytearray(fragment)
+
+                if len(self.buf) >= nbytes:
+                    break
+                elif len(fragment) < 1:
+                    break
+                else:
+                    time.sleep(pause)
+            except socket.timeout:
+                pass
+
+        if timeout is not None:
+            conn.settimeout(orig_timeout)
 
--- plcsimulator/ModbusModule.py
+++ plcsimulator/ModbusModule.py
@@ -6,6 +6,7 @@
 ###############################################################################
 
 import logging
+import socket
 
 from plcsimulator.BaseFieldbusModule import BaseFieldbusModule
 from plcsimulator.FieldbusMessage import FieldbusMessage
@@ -13,17 +14,17 @@ from plcsimulator.FieldbusMessage import FieldbusMessage
 class ModbusModule(BaseFieldbusModule):
 
     DEFAULTS = {
-        'blen': 520,
+        'min_msg_len': 12,
         'word_nbytes': 2,
         'word_mem_section': 'words16',
         'functions': {
             '0x03': {
-                'name': 'read_holding_registers',
+                'name': 'read holding registers',
                 'code': 0x03,
                 'base_addr': 40000
             },
             '0x10': {
-                'name': 'preset_multiple_registers',
+                'name': 'preset multiple registers',
                 'code': 0x10,
                 'base_addr': 40000
             }
@@ -31,13 +32,32 @@ class ModbusModule(BaseFieldbusModule):
         'exception_flag': 0x80
     }
 
-    def __init__(self, id):
-        super().__init__(id)
-        self.blen = self.DEFAULTS['blen']
+    def recv_request_fragment(self, request, nbytes):
+        request.recv_fragment(self.conn, nbytes, flags=socket.MSG_WAITALL)
+
+        if len(request.buf) < nbytes:
+            raise ValueError('Request length too short (received {} bytes, expected {} bytes)'.format(len(request.buf), nbytes))
+
+        return request
+
+    def get_request(self):
+        request = FieldbusMessage(0)
+
+        # Get the minimum message that is common to all Modbus requests
+        self.recv_request_fragment(request, self.DEFAULTS['min_msg_len'])
+
+        # For write requests, we also need to read the data payload.  This
+        # consists of the data length byte and the data
+
+        if request.buf[7] == self.DEFAULTS['functions']['0x10']['code']:
+            nwords = request.make_word(10, 11)
+            msg_nbytes = self.DEFAULTS['min_msg_len'] + 1 + nwords * self.DEFAULTS['word_nbytes']
+            self.recv_request_fragment(request, msg_nbytes)
+
+        return request
 
     def process_request(self):
-        request = FieldbusMessage(self.blen)
-        request.buf = self.conn.recv(self.blen)
+        request = self.get_request()
 
         if request.buf[7] == self.DEFAULTS['functions']['0x03']['code']:
             self.service_read_holding_registers_request(request)
@@ -47,14 +67,14 @@ class ModbusModule(BaseFieldbusModule):
             self.service_unknown_request(request)
 
     def service_read_holding_registers_request(self, request):
-        base_addr = self.DEFAULTS['functions']['0x03']['base_addr']
+        log_prefix = '{}: {}:'.format(self.id, self.DEFAULTS['functions']['0x03']['name'])
         addr = request.make_word(8, 9)
         nwords = request.make_word(10, 11)
 
         data = self.memory_manager.get_data(section=self.DEFAULTS['word_mem_section'], addr=addr, n=nwords)
         data_nbytes = nwords * self.DEFAULTS['word_nbytes']
 
-        logging.debug("Read holding registers: addr = {}, nwords = {}, data_nbytes = {}, data = {}".format(addr, nwords, data_nbytes, data))
+        logging.debug('{} addr = {}, nwords = {}, data_nbytes = {}, data = {}'.format(log_prefix, addr, nwords, data_nbytes, data))
 
         response = FieldbusMessage(9 + data_nbytes)
 
@@ -62,13 +82,13 @@ class ModbusModule(BaseFieldbusModule):
         response.buf[8] = data_nbytes
         response.buf[9:9+data_nbytes+1] = data
 
-        logging.debug("Read holding registers: response: {}".format(response.buf))
+        logging.debug('{} response: {}'.format(log_prefix, response.buf))
         self.conn.sendall(response.buf)
 
         return response
 
     def service_preset_multiple_registers_request(self, request):
-        base_addr = self.DEFAULTS['functions']['0x10']['base_addr']
+        log_prefix = '{}: {}:'.format(self.id, self.DEFAULTS['functions']['0x10']['name'])
         addr = request.make_word(8, 9)
         nwords = request.make_word(10, 11)
         data_nbytes = request.buf[12]
@@ -76,7 +96,7 @@ class ModbusModule(BaseFieldbusModule):
 
         self.memory_manager.set_data(section=self.DEFAULTS['word_mem_section'], addr=addr, n=nwords, data=data)
 
-        logging.debug("Preset multiple registers: addr = {}, nwords = {}, data_nbytes = {}, data = {}".format(addr, nwords, data_nbytes, data))
+        logging.debug('{} addr = {}, nwords = {}, data_nbytes = {}, data = {}'.format(log_prefix, addr, nwords, data_nbytes, data))
 
     def service_unknown_request(self, request):
         request.buf[7] |= self.DEFAULTS['exception_flag']
