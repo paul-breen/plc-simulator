d3cb7e9ea9a7c3f89d2e214890a3fdfd49573032
Paul Breen <pbree@bas.ac.uk>
2018-07-17 18:18:26 +0100

    Add initial simulation functions to IoManager

--- plcsimulator/IoManager.py
+++ plcsimulator/IoManager.py
@@ -5,6 +5,10 @@
 # Date:    2018-07-17
 ###############################################################################
 
+import logging
+import threading
+import time
+
 class IoManager(object):
 
     DEFAULTS = {
@@ -16,8 +20,69 @@ class IoManager(object):
         self.memory_manager = memory_manager
 
     def init_io(self):
-        ### TEMP. DURING DEV. ###
-        for i in range(0, 100):
-            data = i.to_bytes(2, byteorder=self.DEFAULTS['byteorder'])
-            self.memory_manager.set_data(section='words16', addr=i, n=2, data=data)
+        for conf in self.conf['simulations']:
+            id = self.get_simulation_id(conf)
+            logging.info('Starting simulation {}'.format(id))
+
+            # N.B.: args expects a tuple, hence the trailing comma.  Setting
+            # the thread's daemon status to True, ensures that the thread will
+            # terminate when the application main thread is terminated
+            simulation = threading.Thread(target=self.run_simulation, args=(conf,))
+            simulation.daemon = True
+            simulation.start()
+
+    def get_simulation_id(self, conf):
+        id = ''
+
+        try:
+            # Generate an ID for this simulation from its configuration
+            mem_id = ':'.join([str(x) for x in conf['memspace'].values()])
+            func_id = ':'.join([str(x) for x in conf['function'].values()])
+            id = ':'.join([mem_id, func_id])
+        except KeyError:
+            pass
+
+        return id
+
+    def run_simulation(self, conf):
+        sources = {
+            'counter': 0,
+            'binary': 0
+        }
+
+        while True:
+            data = self.simulate_data(conf, sources)
+            self.memory_manager.set_data(**conf['memspace'], data=data)
+
+            try:
+                time.sleep(conf['pause'])
+            except KeyError:
+                pass
+
+    def simulate_data(self, conf, sources):
+        data = bytearray(0)
+
+        try:
+            wlen = self.memory_manager.get_section_word_len(conf['memspace']['section'])
+            nwords = int(conf['memspace']['nwords'])
+
+            if conf['function']['type'] == 'counter':
+                value = sources['counter']
+                sources['counter'] += 1
+                b = value.to_bytes(wlen, byteorder=self.DEFAULTS['byteorder'])
+
+                for i in range(nwords):
+                    data += b
+            elif conf['function']['type'] == 'binary':
+                value = sources['binary']
+                sources['binary'] = int(not value)
+                b = value.to_bytes(wlen, byteorder=self.DEFAULTS['byteorder'])
+
+                for i in range(nwords):
+                    data += b
+
+        except KeyError:
+            pass
+
+        return data
 
