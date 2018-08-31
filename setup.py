14d524898cf1c6d4ef24dc31673e136e4e762160
Paul Breen <pbree@bas.ac.uk>
2018-08-31 14:43:57 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup
 
 setup(name='plc-simulator',
-      version='0.1',
+      version='0.2',
       description='PLC Simulator',
       url='https://github.com/paul-breen/plc-simulator',
       author='Paul Breen',
