c958b1b63bd42b118d7f1c1fadd43ab6265eb5c9
Paul Breen <pbree@bas.ac.uk>
2025-08-17 17:44:08 +0100

    Bump version

--- plcsimulator/__init__.py
+++ plcsimulator/__init__.py
@@ -4,7 +4,7 @@ Package for defining and running a PLC simulation environment
 The package contains classes for instantiating a soft PLC.  It can be used in development and testing as part of a simulation environment.  It provides support for simulating Modbus devices, but can be extended to support other field bus protocols.  All communications are over TCP/IP.
 """
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 PROGNAME = __name__
 
--- tests/test_plcsimulator.py
+++ tests/test_plcsimulator.py
@@ -7,5 +7,5 @@ import plcsimulator
 base = os.path.dirname(__file__)
 
 def test_version():
-    assert plcsimulator.__version__ == '0.4.0'
+    assert plcsimulator.__version__ == '0.5.0'
 
